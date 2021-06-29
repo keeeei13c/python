08942e65f19753ddf2085a8fd301dfd6790e17df
keeeei13c <kei.121003f@icloud.com>
2021-06-29 18:16:17 +0900

    ⚡️ practice programing

---
+++ udemy/ex.py
@@ -0,0 +1,128 @@
+# import math
+"1"
+# print('こんにちは、私は、学生です')
+"2"
+# print('こんにちは、\nお元気ですか')
+"3"
+# x = 'x = 8'
+# y = 'y = 2'
+# print(x,';',y)
+
+# x = 8
+# y = 2
+"4"
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x-y)
+# print(x%y)
+"5"
+from sys import stdin
+input = stdin.readline
+# a,b= map(int,input().split())
+# c = a + b
+# print(c)
+# c = a -b
+# print(c)
+# c = a * b
+# print(c)
+
+# if c >= 50:
+#     print(1)
+# else:
+#     print(0)
+
+# if c > 30:
+#     print(2)
+# else:
+#     print(1)
+"6"
+# NUM = int(input(1000))
+# if NUM % 2 == 0:
+#     print('偶数')
+# else:
+#     print('奇数')
+"7"
+# NUM = int(input())
+
+# if NUM > 100:
+#     print('範囲外です')
+# elif NUM % 2 == 0:
+#     print('偶数')
+# else:
+#     print('奇数')
+"8"
+
+# NUM = int(input())
+
+# if NUM % 3 == 0:
+#   print('3の倍数です')
+# elif NUM % 2 == 0:
+#     print('偶数')
+# else:
+#     print('奇数')
+"9"
+# a,b,c = 178,165,173
+# ans = (a + b + c)/3
+# print(ans)
+
+s = {0:"日曜日",1:"月曜日",2:"火曜日",3:"水曜日",4:"木曜日",5:"金曜日",6:"日曜日"}
+
+for i in s.keys():
+    print(i)
+print()
+
+
+# pi = math.pi
+# r = 79.23
+# s = r * r * pi
+
+# print(s)
+
+"16 ok"
+# for i in range(1,100):
+#     if i % 7 == 0: 
+#         print(i)
+
+
+"18"
+# n = int(input())
+# if n > 80:
+#     print('A')
+# elif n > 60:
+#     print('B')
+# elif n > 40:
+#     print('C')
+# else:
+#     print('D')
+
+# age = input()
+# weight = input()
+# height = input()
+
+# if age > 25 and height > 173 and weight > 75:
+#     print('ok')
+# elif age < 25 and height > 173 and weight > 75:
+#     print('年齢over')
+# elif age > 25 and height < 173 and weight > 75:
+#     print('身長が不足')
+# else:
+#     print('体重が不足')
+"22 ok"
+# print("体重を入力してください")
+# weight = int(input())
+# print("身長を入力してください")
+# height = int(input())
+# bmi = weight / (height * height)
+# bmi = bmi * 10000
+# bmi = round(bmi, 1)
+# print(bmi)
+# if bmi > 30:
+#     print('肥満')
+# elif bmi > 25:
+#     print('肥満')
+# elif bmi > 18.6:
+#     print('正常')
+# else:
+#     print('痩せている')
+
