35712aba5da4bca2e46c5cb4984fc5a2500f5423
keeeei13c <kei.121003f@icloud.com>
2021-07-02 20:17:49 +0900

    rechallenge selection_sort

--- algo/select_sort.py
+++ algo/select_sort.py
@@ -1,17 +1,18 @@
 from typing import List
-
+import random
 def selection_sort(numbers: List[int]) -> List[int]:
     len_numbers = len(numbers)
-    for i in range(numbers):
+    for i in range(len_numbers):
         min_idx = i
-        for j in range(i+1 , len_numbers):
+        for j in range(i+1, len_numbers):
             if numbers[min_idx] > numbers[j]:
                 min_idx = j
 
-    numbers[i], numbers[min_idx] = numbers[min_idx], numbers[i]
+        numbers[i],numbers[min_idx] = numbers[min_idx],numbers[i]
 
     return numbers
 
 if __name__ == '__main__':
-    nums = [2, 5, 1, 8, 7, 3]
-    print(bubble_sort(nums))
+    nums = [random.randint(0,1000) for _ in range(0,10)]
+    print(selection_sort(nums))
+
